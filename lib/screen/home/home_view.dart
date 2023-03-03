026e7dad6fefb80b9d4dc66e8f221797fb6e3b8e
zsakvo <gdwqsp@gmail.com>
2023-03-03 08:50:50 +0800

    fix:app exit

--- lib/screen/home/home_view.dart
+++ lib/screen/home/home_view.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter_exit_app/flutter_exit_app.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:go_router/go_router.dart';
 // import 'package:go_router_flow/go_router_flow.dart';
@@ -73,8 +74,9 @@ class _HomeViewState extends ConsumerState<HomeView> with AutomaticKeepAliveClie
             ref.read(verificationItemsProvider.notifier).refresh();
           });
         } else {
-          SystemNavigator.pop();
-          exit(0);
+          // SystemNavigator.pop();
+          // exit(0);
+          FlutterExitApp.exitApp();
         }
       }
       return () {};
