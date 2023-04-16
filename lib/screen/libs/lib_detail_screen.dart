8f7a4c2adbf087a4a97c4b9b7481eaa2e25e53af
zsakvo <gdwqsp@gmail.com>
2023-04-17 02:50:08 +0800

    update: open source libs

--- lib/screen/libs/lib_detail_screen.dart
+++ lib/screen/libs/lib_detail_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
+import 'package:html_unescape/html_unescape.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:http/http.dart' as http;
 
 class LibDetailView extends HookConsumerWidget {
   const LibDetailView({super.key, required this.lib});
@@ -9,7 +10,26 @@ class LibDetailView extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
     Future<String> getLicenseString() async {
-      return await rootBundle.loadString("assets/licenses/$lib");
+      RegExp regex = RegExp(r'<pre.*?>([\s\S]*?)<\/pre>', multiLine: true, dotAll: true);
+
+      // return await rootBundle.loadString("assets/licenses/$lib");
+      final response = await http.get(Uri.parse('https://pub.dev/packages/$lib/license'));
+      if (response.statusCode == 200) {
+        // 获取网页源代码
+        String source = response.body;
+        Iterable<Match> matches = regex.allMatches(source);
+        if (matches.isNotEmpty) {
+          // 提取匹配的文本
+          var unescape = HtmlUnescape();
+          return unescape.convert(matches.map((match) => match.group(1)).join(''));
+        } else {
+          return 'License fetch failed.';
+        }
+      } else {
+        // 处理错误
+        // throw Exception('Failed to fetch web page');
+        return 'License fetch failed.';
+      }
     }
 
     final licenseString = useFuture(useMemoized(() => getLicenseString()), initialData: "");
